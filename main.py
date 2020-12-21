54ff0620e9b85dbcddb975816daa7098f3f055d2
Tareq Judeh <tareq.judeh@outlook.com>
2020-12-21 17:10:36 +0200

    Added a Search button, that displays website saved information.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import Tk, Canvas, PhotoImage, Label, Entry, Button, END, \
     messagebox
 import pyperclip
 from random import randint, shuffle, choice
+import json
 from characters import letters, numbers, symbols
 
 
@@ -10,17 +11,11 @@ from characters import letters, numbers, symbols
 
 def generate_password():
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
-    print(password_letters)
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
-    print(password_numbers)
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    print(password_symbols)
 
     password_list = password_letters + password_numbers + password_symbols
     shuffle(password_list)
-
-    # Hashing the password:
-    hashed_password = [letter.replace(letter, "*") for letter in password_list]
 
     password = "".join(password_list)
     password_entry.insert(0, password)
@@ -35,6 +30,13 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0:
         return messagebox.showerror(title="Error",
@@ -54,15 +56,58 @@ def save():
                                                    f"Email: {email}\n"
                                                    f"Password: {password}\n"
                                                    f"Press Ok to save.")
+        # Save/Write to data.json:
         if save_data:
-            with open("data.txt", mode="a") as file:
-                file.write(f"{website} | {email} | {password} \n")
 
-            website_entry.delete(0, END)
-            email_entry.delete(0, END)
-            password_entry.delete(0, END)
-            website_entry.focus()
-            print("New information saved to file.")
+            try:
+                # Update data.json:
+                with open("data.json", mode="r") as data_file:
+                    # Reading data:
+                    data = json.load(data_file)
+
+            except FileNotFoundError:
+                with open("data.json", mode="w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+
+            else:
+                # Updating data
+                data.update(new_data)
+
+                # Saving data:
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+
+            finally:
+                website_entry.delete(0, END)
+                email_entry.delete(0, END)
+                password_entry.delete(0, END)
+                website_entry.focus()
+                print("New information saved to file.")
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+def find_password():
+    website = website_entry.get()
+
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        print("File not found!")
+        messagebox.showerror(title="Error", message="Data file not found!")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(
+                title=website,
+                message=f"Email: {email}\nPassword:{password}"
+            )
+        else:
+            messagebox.showinfo(title="", message="Website not found!")
+            print("Website not found!")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -70,6 +115,8 @@ def save():
 YELLOW = "#f7f5dd"
 
 window = Tk()
+window.option_add('*Dialog.msg.font', 'Arial 10')
+
 window.title("Password Manager")
 window.config(padx=40, pady=40, bg=YELLOW)
 
@@ -77,7 +124,6 @@ canvas = Canvas(width=200, height=200, bg=YELLOW, highlightthickness=0)
 lock_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_image)
 canvas.grid(column=1, row=0)
-
 
 # Labels:
 website_label = Label(text="Website:", bg=YELLOW)
@@ -90,7 +136,7 @@ password_label = Label(text="Password", bg=YELLOW)
 password_label.grid(column=0, row=3, sticky="W")
 
 # Entries(text fields):
-website_entry = Entry(width=43, bg=YELLOW)
+website_entry = Entry(width=23, bg=YELLOW)
 website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2, sticky="W")
 
@@ -98,15 +144,18 @@ email_entry = Entry(width=43, bg=YELLOW)
 email_entry.insert(0, "example@email.com")
 email_entry.grid(column=1, row=2, columnspan=2, sticky="W")
 
-password_entry = Entry(width=18, bg=YELLOW)
+password_entry = Entry(width=23, bg=YELLOW)
 password_entry.grid(column=1, row=3, sticky="W")
 
 # Buttons:
 
+search = Button(text="Search", padx=11, width=15, bg=YELLOW, command=find_password)
+search.grid(column=2, row=1, sticky="W")
+
 add_btn = Button(text="Add", padx=10, width=41, bg=YELLOW, command=save)
 add_btn.grid(column=1, row=4, columnspan=2, sticky="W")
 
-gen_pass_btn = Button(text="Generate Password", bg=YELLOW,
+gen_pass_btn = Button(text="Generate Password", padx=11, bg=YELLOW,
                       command=generate_password, width=15)
 gen_pass_btn.grid(column=2, row=3, sticky="W")
 
